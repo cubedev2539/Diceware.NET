34c73bdfc0de6170221bb1eed99c6690c20df30a
cubedev2539 <cubedev2539@gmail.com>
2016-12-31 00:14:10 -0700

    Switch list used to online version of Diceware 8k, changed randomization method.

--- Diceware.NET/PassCreate.cs
+++ Diceware.NET/PassCreate.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,11 +14,11 @@ namespace Diceware.NET
 {
     public partial class PassCreate : Form
     {
-        public Dictionary<int, string> FormattedDiceware = new Dictionary<int, string>();
+        public string[] Diceware;
         public RNGCryptoServiceProvider rand = new RNGCryptoServiceProvider();
-        public PassCreate(Dictionary<int,string> dicewareList)
+        public PassCreate(string[] dicewareList)
         {
-            FormattedDiceware = dicewareList;
+            Diceware = dicewareList;
             InitializeComponent();
             comboBox1.SelectedIndex = 0;
         }
@@ -28,44 +29,23 @@ namespace Diceware.NET
             List<string> WordList = new List<string>();
             int TempInt = 0;
             byte[] ByteOut = new byte[1];
-            int[] Dice = new int[5];
+            BitArray Bits = new BitArray(13);
+            int[] Number = new int[1];
             for (int l = 0; l < Loops; l++)
             {
-                for (int i = 0; i < 5; i++)
+                for (int i = 0; i < 13; i++)
                 {
-                    InvalidRoll:
-                    rand.GetBytes(ByteOut);
+                    rand.GetNonZeroBytes(ByteOut);
                     if (BitConverter.IsLittleEndian)
                         Array.Reverse(ByteOut);
                     TempInt = Convert.ToInt32(ByteOut[0]);
-                    if (TempInt < 252)
-                    {
-                        switch (TempInt % 6)
-                        {
-                            case 0:
-                                Dice[i] = 6;
-                                break;
-                            case 1:
-                                Dice[i] = 1;
-                                break;
-                            case 2:
-                                Dice[i] = 2;
-                                break;
-                            case 3:
-                                Dice[i] = 3;
-                                break;
-                            case 4:
-                                Dice[i] = 4;
-                                break;
-                            case 5:
-                                Dice[i] = 5;
-                                break;
-                        }
-                    }
+                    if (Enumerable.Range(0, 127).Contains(TempInt))
+                        Bits[i] = false;
                     else
-                        goto InvalidRoll;
+                        Bits[i] = true;
                 }
-                WordList.Add($"{FormattedDiceware[Convert.ToInt32($"{Dice[0]}{Dice[1]}{Dice[2]}{Dice[3]}{Dice[4]}")]}");
+                Bits.CopyTo(Number, 0);
+                WordList.Add(Diceware[Number[0]]);
             }
             string SeparatorString = string.Empty;
             switch(comboBox1.SelectedIndex)
